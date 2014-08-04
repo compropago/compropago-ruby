8e484d129e1d9f1369f839a618811c69d9eaed5e
robertomiranda <rjmaltamar@gmail.com>
2014-08-04 15:02:20 -0500

    Listing Available Stores

--- lib/compropago/charge.rb
+++ lib/compropago/charge.rb
@@ -2,5 +2,9 @@ module Compropago
   class Charge < Base
      get :find, "/charges/:id"
      post :create, "/charges"
+
+     STORES = %w(OXXO WALMART SAMS_CLUB ELEKTRA EL_PORTON
+     FARMACIA_ESQUIVAR SEVEN_ELEVEN SORIANA BODEGA_AURRERA
+     COPPEL FARMACIA_BENAVIDES FARMACIA_GUADALAJARA FARMACIA_ESQUIVAR)
   end
 end
