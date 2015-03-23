14eb34bfddcbdea094461e6c31abd788defaa22a
Francisco Soto <bobby@hellocode.mx>
2015-03-23 11:58:56 -0700

    *BUGFIX* Warnings about duplicate keys.

--- lib/compropago/client.rb
+++ lib/compropago/client.rb
@@ -11,7 +11,7 @@ module Compropago
 
   	  #defaults
   	  options[:base_uri] ||= BASE_URI
-  	  @base_uri = options[:base_uri]	
+  	  @base_uri = options[:base_uri]
   	end
 
   	def create_charge(product_price, product_name, customer_name, customer_email, payment_type, product_id=nil, image_url=nil)
@@ -22,17 +22,15 @@ module Compropago
   	  request = Net::HTTP::Post.new(uri.request_uri)
   	  request.basic_auth @api_key, ''
   	  params = { "product_price" => product_price,
-  	  			 "product_name" => product_name,
-  	  			 "customer_name" => customer_name,
-  	  			 "customer_email" => customer_email,
-  	  			 "payment_type" => payment_type,
-  	  			 "product_id" => product_id,
-  	  			 "image_url" => image_url,
-  	  			 "product_id" => product_id,
-  	    		 "image_url" => image_url
-  	  		  }
+  	  			     "product_name" => product_name,
+  	  			     "customer_name" => customer_name,
+  	  			     "customer_email" => customer_email,
+  	  			     "payment_type" => payment_type,
+  	  			     "product_id" => product_id,
+  	    		     "image_url" => image_url
+  	  		     }
   	  request.set_form_data(params)
-  	  response = http.request(request)
+  	  http.request(request)
   	end
 
   	def verify_charge(payment_id)
@@ -42,7 +40,7 @@ module Compropago
   	  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
   	  request = Net::HTTP::Get.new(uri.request_uri)
   	  request.basic_auth @api_key, ''
-  	  response = http.request(request)
+  	  http.request(request)
   	end
 
   	def send_payment_instructions(payment_id, customer_phone, customer_company_phone)
@@ -53,11 +51,11 @@ module Compropago
   	  request = Net::HTTP::Post.new(uri.request_uri)
   	  request.basic_auth @api_key, ''
   	  params = { "payment_id" => payment_id.to_s,
-  	  			 "customer_phone" => customer_phone.to_s,
-  	  			 "customer_company_phone" => customer_company_phone
-  	  		   }
+  	  			     "customer_phone" => customer_phone.to_s,
+  	  			     "customer_company_phone" => customer_company_phone
+  	  		     }
   	  request.set_form_data(params)
-  	  response = http.request(request)
+  	  http.request(request)
   	end
   end
 end
