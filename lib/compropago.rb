4fd486ff7dfa330282fe11621cff8fe4a7da39e0
Eduardo Aguilar Yépez <dante.aguilar41@gmail.com>
2016-07-13 13:09:34 -0500

    Autoload updated

--- lib/compropago.rb
+++ lib/compropago.rb
@@ -1,2 +1,8 @@
 require 'compropagogem/client'
 require 'compropagogem/service'
+
+require 'compropagogem/factory/factory'
+
+require 'compropagogem/tools/validations'
+
+require 'compropagogem/models/eval_auth_info'
